5c41118399aa97a963287e6bbf6052cd4b826bee
Jin <JINWEN_SHIEH@ms.dlp.com.t>
2021-12-24 12:55:35 +0800

    mergeIntervals stock

---
+++ medium/mergeIntervals.js
@@ -0,0 +1,30 @@
+// Given an array of intervals where intervals[i] = [starti, endi],
+// merge all overlapping intervals,
+// and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+function mergeIntervals (intervals) {
+  // 剔除掉不合法的輸入
+  for (let i=0; i<intervals.length; i++) {
+    if (intervals[i].length !== 2) {
+      console.log('每組數字需兩個一組，請重新輸入。');
+      return;
+    }
+  }
+  // 先將數組排序好（雙維皆小->大）
+  let sortedIntervals = intervals.sort((a, b) => a[0] - b[0]).map(interval => interval.sort((a, b) => a - b));
+
+  // 開始 compare
+  let index = 0;
+  if (index + 1 === sortedIntervals.length) return sortedIntervals;
+  while(sortedIntervals[index+1][0] <= sortedIntervals[index][1] && sortedIntervals[index][1] <= sortedIntervals[index+1][1]) {
+    sortedIntervals.splice(index, 2, [sortedIntervals[index][0], sortedIntervals[index+1][1]]);
+    console.log(sortedIntervals);
+    console.log(index+1, sortedIntervals.length)
+  }
+
+  index++;
+};
+
+// 期待結果：[[2, 10], [11, 17]]
+// 目前只跟下一個比沒辦法滿足結果，所以可能要在 while 內寫 for 回圈解決
+mergeIntervals([[2, 9], [8, 10], [10, 7], [11,13], [14, 17], [12, 15]]);
