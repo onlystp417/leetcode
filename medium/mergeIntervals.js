7d13503e9248827f352b4abab5458ecb39f767df
Jin <JINWEN_SHIEH@ms.dlp.com.t>
2021-12-24 13:05:49 +0800

    mergeIntervals Exception handling

--- medium/mergeIntervals.js
+++ medium/mergeIntervals.js
@@ -5,8 +5,18 @@
 function mergeIntervals (intervals) {
   // 剔除掉不合法的輸入
   for (let i=0; i<intervals.length; i++) {
-    if (intervals[i].length !== 2) {
-      console.log('每組數字需兩個一組，請重新輸入。');
+    if (Array.isArray(intervals)) {
+        const isIntervalAllArray = intervals.find(interval => !Array.isArray(interval))
+        if(isIntervalAllArray) {
+          console.log('需輸入二維陣列');
+          return;
+        }
+        if (intervals[i].length !== 2) {
+          console.log('每組數字陣列須兩個一組。');
+          return;
+        }
+      } else {
+      console.log('需輸入陣列。');
       return;
     }
   }
@@ -27,4 +37,4 @@ function mergeIntervals (intervals) {
 
 // 期待結果：[[2, 10], [11, 17]]
 // 目前只跟下一個比沒辦法滿足結果，所以可能要在 while 內寫 for 回圈解決
-mergeIntervals([[2, 9], [8, 10], [10, 7], [11,13], [14, 17], [12, 15]]);
+mergeIntervals([[2, 9], [8, 10], [7, 10, 15], [11,13], [14, 17], [12, 15]]);
